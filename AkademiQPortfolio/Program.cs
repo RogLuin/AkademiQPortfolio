bf16a2e34c624e0698efaf4130f75b9c0a296ef2
Görkem Dikbakan <gorkemdikbakan@gmail.com>
2026-01-01 21:46:11 +0300

    Düzenlemeler Yapıldı

--- AkademiQPortfolio/Program.cs
+++ AkademiQPortfolio/Program.cs
@@ -24,6 +24,12 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+);
+
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
--- AkademiQPortfolio/ViewComponents/AdminLayoutViewComponents/_AdminLayoutSidebarComponentPartial.cs
+++ AkademiQPortfolio/ViewComponents/AdminLayoutViewComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -2,7 +2,7 @@
 
 namespace AkademiQPortfolio.ViewComponents.AdminLayoutViewComponents
 {
-    public class _AdminLayoutSidebarComponentPartial: ViewComponent
+    public class _AdminLayoutSidebarComponentPartial : ViewComponent
     {
         public IViewComponentResult Invoke()
         {
